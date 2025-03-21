c5c7fef7a469d5d9809327c22b2eaf72b6ba42b4
23021402 <23021402@vnu.edu.vn>
2025-03-21 22:55:28 +0700

    Create Bai_01.cpp

---
+++ Lect4_Assignments/Bai_01.cpp
@@ -0,0 +1,3 @@
+// Hai thuộc tính quan trọng của một hàm băm:
+// Tính đồng nhất (Uniformity) – Hàm băm nên phân phối giá trị băm một cách đều đặn để giảm thiểu xung đột (collisions).
+// Tính không thể đảo ngược (Preimage Resistance) – Không thể dễ dàng tìm ra giá trị đầu vào từ giá trị băm
