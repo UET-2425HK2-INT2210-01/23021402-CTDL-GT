ebb57a5b23c1f27b1dedc0689cf8b687853ec347
23021402 <23021402@vnu.edu.vn>
2025-04-19 23:43:39 +0700

    Create Bai_01.cpp

---
+++ Lect7.0_Assignments/Bai_01.cpp
@@ -0,0 +1,21 @@
+#include<iostream> // Thư viện nhập xuất
+using namespace std; // Dùng không gian tên chuẩn
+
+// Hàm tính tổng từ 1 đến n (dùng đệ quy)
+int Tong(int n){
+    if(n == 1) 
+        return 1; // Nếu n là 1 thì trả về 1
+    else 
+        return n + Tong(n - 1); // Tổng = n + tổng từ 1 đến (n-1)
+}
+
+int main(){
+    int n;
+    cout << "Nhập n: "; // Nhập số nguyên n
+    cin >> n;
+
+    int ketqua = Tong(n); // Gọi hàm tính tổng
+    cout << ketqua; // In kết quả
+
+    return 0; // Kết thúc chương trình
+}
