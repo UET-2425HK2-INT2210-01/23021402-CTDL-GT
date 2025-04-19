d7dbcdbfe272df09c5f059212165256ccc199fc5
23021402 <23021402@vnu.edu.vn>
2025-04-19 23:49:36 +0700

    Create Bai_02.cpp

---
+++ Lect7.0_Assignments/Bai_02.cpp
@@ -0,0 +1,21 @@
+#include<iostream> // Thư viện nhập xuất
+using namespace std;
+
+// Hàm tính giai thừa của n (n!)
+int Tinhgiaithua(int n){
+    if(n == 1)
+        return 1; // Giai thừa của 1 là 1
+    else 
+        return n * Tinhgiaithua(n - 1); // Giai thừa = n * (n-1)!
+}
+
+int main(){
+    int n;
+    cout << "Nhap n: "; // Nhập số nguyên n
+    cin >> n;
+
+    int Ketqua = Tinhgiaithua(n); // Gọi hàm tính giai thừa
+    cout << Ketqua; // In kết quả
+
+    return 0; // Kết thúc chương trình
+}
